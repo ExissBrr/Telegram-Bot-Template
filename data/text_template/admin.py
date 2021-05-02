261e18db49fb1d9dd0b13d62502dcd28870c55d3
Vlad <veenrokdalv@protonmail.com>
2021-05-02 23:41:41 +0300

    Template v1 update

---
+++ data/text_template/admin.py
@@ -0,0 +1,5 @@
+with open("data/text_template/admin/welcome.txt", encoding="UTF-8") as file:
+    welcome = file.read()
+
+with open("data/text_template/admin/help.txt", encoding="UTF-8") as file:
+    help = file.read()
