21f81057b9073a443f3c8f195f4195cbe166848a
Vlad <veenrokdalv@protonmail.com>
2021-05-03 09:24:11 +0300

    Template v1 update -Storage memory заменен на Redis memory

--- loader.py
+++ loader.py
@@ -1,12 +1,12 @@
 from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aiogram.types import ParseMode
 from gino import Gino
 
 from data.config import TOKEN_BOT
 
 DATABASE = Gino()
-STORAGE = MemoryStorage()
+STORAGE = RedisStorage2()
 BOT = Bot(token=TOKEN_BOT, parse_mode=ParseMode.HTML)
 DP = Dispatcher(bot=BOT, storage=STORAGE)
 
